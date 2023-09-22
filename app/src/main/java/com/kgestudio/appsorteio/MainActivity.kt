6f0455fc689be4ca0e7b52bf944039b577deea7a
GabrielMouraKT <gabrieljknd@gmail.com>
2023-09-22 09:36:45 -0300

    Codigo

--- app/src/main/java/com/kgestudio/appsorteio/MainActivity.kt
+++ app/src/main/java/com/kgestudio/appsorteio/MainActivity.kt
@@ -2,10 +2,17 @@ package com.kgestudio.appsorteio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun sorteio(view: View){
+    val textoResultado = findViewById<TextView>(R.id.text_resultado)
+        val numero = java.util.Random().nextInt(11)//0...10
+    textoResultado.setText("numero gerado $numero")
     }
 }
